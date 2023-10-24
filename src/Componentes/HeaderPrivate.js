9267256ec67b582b9b44341ed967dca7be7893c6
Endercarias <endercarias@gmail.com>
2023-10-23 22:00:43 -0600

    Add: Finalizado

--- src/Componentes/HeaderPrivate.js
+++ src/Componentes/HeaderPrivate.js
@@ -6,14 +6,24 @@ const HeaderPrivate = () => {
         localStorage.removeItem('token')
         window.location = '/carros'
     }
+
+    const token = localStorage.getItem('token')
+    const rol = sessionStorage.getItem('rol')
+
     return (
         <header className="header">
         <h1>Importadora Orellana</h1>
         <nav className="navigation">
         <ul className="menu">
-            <li><a href="/admincar">Carros</a></li>
-            <li><a href="/citasadmin">Citas</a></li>
-            <li><a onClick={()=> cerrarSesion()}>Cerrar sesión</a></li>
+            {
+                token != undefined && rol == '1'
+                ?<>
+                    <li><a href="/admincar">Carros</a></li>
+                    <li><a href="/citasadmin">Citas</a></li>
+                    <li><a onClick={()=> cerrarSesion()}>Cerrar sesión</a></li>
+                </>
+                :""
+            }
         </ul>
         </nav>
         </header>   
